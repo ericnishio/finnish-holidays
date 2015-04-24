8f35575023b71e1ff55c772717612e095c67aa77
Eric Nishio <eric.nishio@nordsoftware.com>
2015-04-24 18:12:09 +0300

    Fix counts

--- holidays.rb
+++ holidays.rb
@@ -11,9 +11,9 @@ class Year
 
   def initialize year
     if year.is_a? Integer and year > 0
-      @year = year.to_s
+      @year = year.to_i
       @holidays = Hash.new { |hash, key| hash[key] = [] }
-      @page = Nokogiri::HTML(open('http://www.webcal.fi/fi-FI/pyhat.php?y=' + @year))
+      @page = Nokogiri::HTML(open('http://www.webcal.fi/fi-FI/pyhat.php?y=' + @year.to_s))
       self.parse_holidays()
     else
       raise "Invalid year: #{year}"
@@ -36,6 +36,8 @@ class Year
 
         holiday = {
           day: day,
+          month: month,
+          year: @year,
           description: description
         }
 
@@ -45,52 +47,70 @@ class Year
   end
 
   def get_holidays
-    return @holidays
+    @holidays
   end
 
   def get_json
-    return self.get_holidays.to_json
+    self.get_holidays.to_json
   end
 
   def get_day date_string
     parts = date_string.split('.')
-    return parts[0].to_i
+    parts[0].to_i
   end
 
   def get_month date_string
      parts = date_string.split('.')
-     return parts[1].to_i
+     parts[1].to_i
   end
 end
 
 class Holidays
+  @y = nil
+  @m = nil
+  @d = nil
   @year = nil
-  @month = nil
-  @day = nil
 
   def initialize
-    @year = Time.now.year
-    @month = Time.now.month
-    @day = Time.now.day
+    @y = Time.now.year
+    @m = Time.now.month
+    @d = Time.now.day
+    @year = Year.new(@y)
   end
 
-  def this_month
-    year = Year.new(@year)
-    return year.get_holidays[@month]
-  end
-
-  def next
-    year = Year.new(@year)
+  def next count = 3
     holidays = Array.new
 
-    if defined? year.get_holidays[@month]
-      year.get_holidays[@month].each do |holiday|
-        if (holiday[:day] > @day)
-          puts holidays.push(holiday)
+    while holidays.length < count
+      if defined? @year.get_holidays[@m]
+        @year.get_holidays[@m].each do |holiday|
+          if (holidays.length < count and holiday[:day] >= @d)
+            holidays.push(holiday)
+          end
         end
+      end
+
+      if holidays.length < count
+        self.goto_next_month()
       end
     end
 
-    return holidays
+    holidays
+  end
+
+  def goto_next_month
+    if @m == 12
+      @m = 1
+      @y += 1
+      @d = 1
+    else
+      @m += 1
+      @d = 1
+    end
+
+    @year = Year.new(@y)
   end
 end
+
+holidays = Holidays.new
+puts holidays.next(5)
