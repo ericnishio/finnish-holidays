de2aebf3a5672583102db3ed45c62fffd355aca8
Eric Nishio <eric@self-learner.com>
2015-04-28 18:47:20 +0300

    Write tests

--- spec/date_utils_spec.rb
+++ spec/date_utils_spec.rb
@@ -35,4 +35,44 @@ describe FinnishHolidays::DateUtils do
     number = FinnishHolidays::DateUtils.zerofy(10)
     expect(number).to be_kind_of(String)
   end
+
+  it 'should determine Easter Sunday in 2015' do
+    expected_date = Date.new(2015, 4, 5)
+    expect(FinnishHolidays::DateUtils.easter_sunday(2015)).to eq(expected_date)
+  end
+
+  it 'should determine Easter Monday in 2016' do
+    expected_date = Date.new(2016, 3, 28)
+    expect(FinnishHolidays::DateUtils.easter_monday(2016)).to eq(expected_date)
+  end
+
+  it 'should determine Good Friday in 2017' do
+    expected_date = Date.new(2017, 4, 14)
+    expect(FinnishHolidays::DateUtils.good_friday(2017)).to eq(expected_date)
+  end
+
+  it 'should determine Ascension Day in 2018' do
+    expected_date = Date.new(2018, 5, 10)
+    expect(FinnishHolidays::DateUtils.ascension_day(2018)).to eq(expected_date)
+  end
+
+  it 'should determine Pentecost in 2019' do
+    expected_date = Date.new(2019, 6, 9)
+    expect(FinnishHolidays::DateUtils.pentecost(2019)).to eq(expected_date)
+  end
+
+  it 'should determine Midsummer Eve in 2020' do
+    expected_date = Date.new(2020, 6, 19)
+    expect(FinnishHolidays::DateUtils.midsummer_eve(2020)).to eq(expected_date)
+  end
+
+  it 'should determine Midsummer Day in 2021' do
+    expected_date = Date.new(2021, 6, 26)
+    expect(FinnishHolidays::DateUtils.midsummer_day(2021)).to eq(expected_date)
+  end
+
+  it "should determine All Saints' Day in 2022" do
+    expected_date = Date.new(2022, 11, 5)
+    expect(FinnishHolidays::DateUtils.all_saints_day(2022)).to eq(expected_date)
+  end
 end
